b99073a4aedeb97fbbb05bb2d18d9cebd94a0193
Yurii <yurastt1@gmail.com>
2021-03-18 23:51:25 +0200

    start screen without fonnts

---
+++ src/StartScreen.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+
+i18n.translations = {
+  'uk-UA': { Hello: 'Почни працювати над собою з нашою персональною дієтою та тренуйся вдома або в тренажерному залі.', Start: 'Почати'},
+  en: { Hello: 'Start to work on yourself with our personal diet & training at home or in a gym.', Start: 'Start'},
+};
+
+i18n.locale = 'en' //Localization.locale;
+
+export const StartScreen = (props) => {
+  return (
+    <View style={styles.container}>
+
+      
+        <Text style={styles.title}>FitMe</Text>
+        <Text style={styles.text}>{i18n.t('Hello')}</Text>
+      
+      
+        <TouchableOpacity
+          onPress={start}
+          style={styles.button}
+        >
+          <Text style={styles.start}>{i18n.t('Start')}</Text>
+        </TouchableOpacity>
+      
+
+    </View>
+  )
+}
+
+const start = () => {
+  console.log(1);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: 'Mont-Heavy',
+    textAlign: 'center',
+    // fontWeight: 800,
+    fontSize: 60
+  },
+  button: {
+    marginBottom: 60,
+    backgroundColor: 'black',
+    borderRadius: 2,
+    width: 323,
+    height: 50,
+    alignItems: 'center',
+    padding: 15,
+  },
+  start: {
+    color: 'white',
+    fontSize: 16,
+  },
+  text: {
+    width: 323,
+    marginBottom: 224,
+    marginTop: 30,
+    fontFamily: 'Mont-SemiBold',
+    // fontWeight: 600
+  }
+  
+});
